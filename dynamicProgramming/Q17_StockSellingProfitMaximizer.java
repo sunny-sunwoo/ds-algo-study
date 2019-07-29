a0fda4eb8180a238015b0fb2565917b92f09df84
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-07-29 00:00:57 -0700

    get max profit for 2 time selling

--- dynamicProgramming/Q17_StockSellingProfitMaximizer.java
+++ dynamicProgramming/Q17_StockSellingProfitMaximizer.java
@@ -12,6 +12,7 @@ package ds_algo_study.dynamicProgramming;
  *     1. build an array with one-time profit for each day
  *     2. keep max from the end.
  *     3. profit = Math.max(profit, max - curr + profit[until prev day])
+ *        => which means, max profit at curr point + max profit up to the prev day.
  *     
  * @author Sunny Park
  *
@@ -24,6 +25,25 @@ public class Q17_StockSellingProfitMaximizer {
             min = Math.min(min, prices[i]);
             profit = Math.max(profit, prices[i] - min);
         }
+        return profit;
+    }
+    
+    public static int getMaxProfit_twotime(int[] prices) {
+        int[] profits = new int[prices.length];
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
+            profits[i] = profit;
+        }
+        
+        int max = prices[prices.length - 1];
+        for (int j = prices.length - 1; j > 0; j--) {
+            max = Math.max(max, prices[j]);
+            profit = Math.max(profits[j], max - prices[j] + profits[j - 1]);
+        }
+        
         return profit;
     }
     
